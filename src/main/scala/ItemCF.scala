dd2bce751921ffacd8b030284323f547cb13b5e9
dingjingmaster <dingjing@live.cn>
2019-08-14 10:31:00 +0800

    更新

--- src/main/scala/ItemCF.scala
+++ src/main/scala/ItemCF.scala
@@ -8,15 +8,14 @@ import scala.collection.mutable.ArrayBuffer
 
 object ItemCF {
   def main(args: Array[String]): Unit = {
-    if (args.length < 4) {
+    if (args.length < 3) {
       println("输入参数不够")
       sys.exit(1)
     }
     val lessPeople = 10                    // 30天内少于次数的书籍不计算
     val giduidPath = args(0)
     val gidmapPath = args(1)
-    val uidmapPath = args(2)
-    val gidRecomPath = args(3)
+    val gidRecomPath = args(2)
 
 //    val giduidPath = "hdfs://10.26.26.145:8020/rs/dingjing/knn/2019-07-29/knn_30_gid_uid/"
 //    val gidRecomPath = "hdfs://10.26.26.145:8020/rs/dingjing/knn/2019-07-29/item_recomm/"
@@ -31,25 +30,38 @@ object ItemCF {
       .setMaster("spark://qd01-tech2-spark001:7077,qd01-tech2-spark002:7077")
     val sc = new SparkContext(conf)
 
-    /* 用户数量 */
-//    val uidnumG = sc.broadcast(sc.textFile(uidmapPath).count())
-
     /* 书籍数量 */
     val gidnumG = sc.broadcast(sc.textFile(gidmapPath).count())
 
-    /* 生成 (gid1|gid2, 用户列表) */
-    val gidVectorRDD = sc.textFile(giduidPath).flatMap(x=>gid_vector(x, gidnumG.value.toInt)).persist(StorageLevel.DISK_ONLY)
+    /* 获取 (gid, List(uid))列表 */
+    val gidUidRDD = sc.textFile(giduidPath).map(x=>x.split("\t"))
+      .filter(_.length >= 2).map(x=>(x(0).toInt, x(1).split("\\{\\]").toList))
+      .filter(_._2.length>=lessPeople).persist(StorageLevel.DISK_ONLY)
 
-    /* 开始计算相似度 */
-    val jaccardRDD = gidVectorRDD.reduceByKey((x, y)=>sim_jaccard(x, y))
+    println("\n********************\n参与计算的物品数量：" + gidUidRDD.count() + "\n********************\n")
+
+    /* 生成 (gid1|gid2, 用户列表) */
+    val gidPairRDD = gidUidRDD.flatMap(x=>{
+      val gid1 = x._1
+      val uidInfo = x._2
+      val buf = ArrayBuffer[Tuple2[String, List[String]]]()
+
+      for (i <- 1 until gidnumG.value.toInt) {
+        if(i > gid1) {
+          buf.append((gid1.toString + "|" + i.toString, uidInfo))
+        } else {
+          buf.append((i.toString + "|" + gid1.toString, uidInfo))
+        }
+      }
+
+      for (i <- buf.toList)
+        yield i
+    })
+
+    val jaccardRDD = gidPairRDD.reduceByKey((x, y)=>sim_jaccard(x, y))
 
     /* 结果保存 */
     jaccardRDD.map(x=>x._1 + "\t" + x._2).repartition(1).saveAsTextFile(gidRecomPath)
-//    val simResult = gidVectorRDD.map(_._1).flatMap(x=>calc_sim(x, gidnumG.value.toInt, gidVectorG.value)).filter(x=>x._1 != 0 && x._2 != 0)
-//      .map(x => x._1.toString + "\t" + x._2.toString + "\t" + x._3.toString).repartition(1).saveAsTextFile(gidRecomPath)
-//    val gidmapG = sc.broadcast(gidmapRDD.collectAsMap())
-//
-//    gidsimRDD.map(x => save_result(x, gidmapG.value)).filter(_!="").repartition(1).saveAsTextFile(gidRecomPath)
   }
 
   def sim_jaccard(x: List[String], y:List[String]): List[String] = {
@@ -81,32 +93,6 @@ object ItemCF {
       yield i
   }
 
-//  def save_result(x: Tuple2[String, Array[Tuple2[String, Double]]], map: Map[String, String]): String = {
-//    var gidx = x._1
-//    val infoy = x._2
-//    var buf = ""`
-//    var gidy = ""
-//    var simy = 0.0
-//    if (map.contains(gidx)) {
-//      gidx = map(gidx)
-//    } else {
-//      return ""
-//    }
-//    for (i <- infoy) {
-//      if(map.contains(i._1)) {
-//        gidy = map(i._1)
-//      } else {
-//        gidy = ""
-//      }
-//      if ("" != buf && "" != gidy) {
-//        buf += "{]" + gidy + "|" + i._2.toString
-//      } else if ("" == buf && "" != gidy) {
-//        buf = gidy + "|" + i._2.toString
-//      }
-//    }
-//    gidx + "\t" + buf
-//  }
-
   /* 计算相似度 */
   def jaccard(x: Vector, y:Vector): Double = {
     var m = (x.toSparse.indices.toSet ++ y.toSparse.indices.toSet).size.toDouble
@@ -115,29 +101,5 @@ object ItemCF {
       m = 1
     }
     z / m
-  }
-
-  def calc_sim(x: Int, max: Int, map: Map[Int, Vector]): List[Tuple3[Int, Int, Double]] = {
-    val arr = ArrayBuffer[Tuple3[Int, Int, Double]]()
-    val gidx:Int = x
-    var gidy:Int = 0
-    val loop = new Breaks
-    var sim:Double = 0.0
-
-    if (!map.contains(gidx)) {
-      arr.append((0, 0, 0.0))
-      return arr.toList
-    }
-    val valx = map(gidx)
-    for (i <- x + 1 to max) {
-      if (map.contains(i)) {
-        gidy = i
-        val valy = map(i)
-        sim = jaccard(valx, valy)
-        arr.append((gidx, gidy, sim))
-      }
-    }
-    for(y <- arr.toList)
-      yield y
   }
 }
